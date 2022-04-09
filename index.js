094528ade48323f5d5e98ee131caea30215b4b5f
Lovanto R <rifkylovanto@gmail.com>
2022-04-09 16:30:01 +0700

    get one and get many data pengunjung

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const configs = require('./configs/configs');
 const mysql = require('mysql2');
 const express = require('express');
 const bodyParser = require('body-parser');
-const ArticleService = require('./services/article_service');
-const ArticleHandler = require('./handlers/article_handler');
+const PengunjungService = require('./services/pengunjung_service');
+const PengunjungHandler = require('./handlers/pengunjung_handler');
 
 const responses = require('./responses/responses');
 
@@ -16,51 +16,48 @@ const mysqlConfig = configs.get('mysql');
  * main() will run the application.
  */
 async function main() {
-  const mysqlPool = mysql.createPool({
-    ...mysqlConfig,
-    timezone: '+07:00'
-  })
-  const promiseMysqlPool = mysqlPool.promise();
+    const mysqlPool = mysql.createPool({
+        ...mysqlConfig,
+        timezone: '+07:00'
+    })
+    const promiseMysqlPool = mysqlPool.promise();
 
-  const articleService = new ArticleService(promiseMysqlPool);
-  const articleHandler = new ArticleHandler(articleService);
+    const pengunjungService = new PengunjungService(promiseMysqlPool);
+    const pengunjungHandler = new PengunjungHandler(pengunjungService);
 
-  const router = express();
-  router.use(bodyParser.urlencoded({ extended: false }));
-  router.use(bodyParser.json());
+    const router = express();
+    router.use(bodyParser.urlencoded({ extended: false }));
+    router.use(bodyParser.json());
 
-  router.get('/v1/articles', articleHandler.getManyArticle.bind(articleHandler));
-  router.post('/v1/articles', articleHandler.createArticle.bind(articleHandler));
-  router.get('/v1/articles/:id', articleHandler.getOneArticle.bind(articleHandler));
-  router.put('/v1/articles/:id', articleHandler.updateArticle.bind(articleHandler));
-  router.delete('/v1/articles/:id', articleHandler.deleteArticle.bind(articleHandler));
-  router.get('/', function (_, res) {
-    responses.success(res, 200, 'OK', 'application is running properly', null, null);
-  });
-
-  const server = router.listen(appConfig.port, function () {
-    console.info('application is running on port', appConfig.port);
-  });
-
-  process.on('SIGINT', function () {
-    server.close(function (err) {
-      if (err) {
-        console.error(err.message);
-      }
-      console.info('server is gracefully shutdown')
+    router.get('/v1/pengunjung', pengunjungHandler.getManyPengunjung.bind(pengunjungHandler));
+    router.get('/v1/pengunjung/:id', pengunjungHandler.getOnePengunjung.bind(pengunjungHandler));
+    router.get('/', function(_, res) {
+        responses.success(res, 200, 'OK', 'application is running properly', null, null);
     });
 
-    mysqlPool.end(function (err) {
-      if (err) {
-        console.error(err.message);
-      }
-      console.info('mysql is gracefully shutdown')
-    })
+    const server = router.listen(appConfig.port, function() {
+        console.info('application is running on port', appConfig.port);
+    });
 
-    setTimeout(function () {
-      process.exit(0);
-    }, 5 * 1000)
-  })
+    process.on('SIGINT', function() {
+        server.close(function(err) {
+            if (err) {
+                console.error(err.message);
+            }
+            console.info('server is gracefully shutdown')
+        });
+
+        mysqlPool.end(function(err) {
+            if (err) {
+                console.error(err.message);
+            }
+            console.info('mysql is gracefully shutdown')
+        })
+
+        setTimeout(function() {
+            process.exit(0);
+        }, 5 * 1000)
+    })
 }
 
 main();
